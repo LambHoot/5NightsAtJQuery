ac86c615c85ea14eda4a53e0ee871580cbef8427
LambHoot <deniskefallinos94@gmail.com>
2016-04-30 20:30:44 -0600

    Camera button
    I don't like CSS position

--- js/cameras.js
+++ js/cameras.js
@@ -1,2 +1,30 @@
 
 //important to note!
+var cameras_open = false;
+$("#camera_screen").addClass("hidden");
+
+function camera_clicked(){
+	//open camera
+	if(!cameras_open){
+		cameras_open = true;
+		$("#camera_screen").removeClass("hidden");
+		$("#c_btn").addClass("active");
+		
+		numDrainers++;
+	}
+	//close camera
+	else{
+		cameras_open = false;
+		$("#camera_screen").addClass("hidden");
+		$("#c_btn").removeClass("active");
+		
+		numDrainers--;
+	}
+	
+}
+
+function resetCameras(){
+	cameras_open = false;
+	$("#camera_screen").addClass("hidden");
+	$("#c_btn").removeClass("active");
+}
